b666bbce95722b229a1d2caef4a6dae949f2c2d7
langdag <lucky10cents@gmail.com>
2021-01-05 14:50:04 +0200

    add active storage

--- app/models/partnership.rb
+++ app/models/partnership.rb
@@ -1,4 +1,6 @@
 class Partnership < ApplicationRecord
     has_many :subscriptions
     has_many :users, through: :subscriptions
+
+    has_many_attached :files
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
     has_many :subscriptions
     has_many :partnerships, through: :subscriptions
+
+    has_one_attached :avatar
 end
