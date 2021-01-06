500787ed7e2c4ae51cfc753514c9a0f04d1fcb3a
langdag <lucky10cents@gmail.com>
2021-01-06 11:20:43 +0200

    add active storage google cloud

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,13 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :email, :name, :avatar
+
+  def avatar
+    if object.avatar.attached?
+      {
+        id: object.avatar.id,
+        name: object.avatar.name,
+        path: object.avatar.url
+      }
+    end
+  end
 end
